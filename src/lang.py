a4e25bf3f1f57962a5bf8226dbdaf68baaa26b96
Clement <clement.desede@gmail.com>
2024-05-27 18:37:06 +0200

    Added condition operators
    TODO: Rewrite the whole thing to allow for jumps and flow control.

--- src/lang.py
+++ src/lang.py
@@ -27,6 +27,14 @@ class TokenTypes(Enum):
 	OP_POP		= iota()
 	OP_PLUS		= iota()
 	OP_MINUS	= iota()
+	OP_MUL		= iota()
+	OP_DIV		= iota()
+	OP_EQ		= iota()
+	OP_NE		= iota()
+	OP_GT		= iota()
+	OP_GE		= iota()
+	OP_LT		= iota()
+	OP_LE		= iota()
 	OP_DUMP		= iota()
 	OP_EXIT		= iota()
 	OP_COUNT	= iota()
@@ -44,23 +52,20 @@ class Token:
 	def __repr__(self):
 		return f"{self.type}{f" {self.value}" if self.value else ""}"
 
-def PUSH(val: Any) -> Token:
-	return Token(TokenTypes.OP_PUSH, val)
-
-def POP() -> Token:
-	return Token(TokenTypes.OP_POP)
-
-def PLUS() -> Token:
-	return Token(TokenTypes.OP_PLUS)
-
-def MINUS() -> Token:
-	return Token(TokenTypes.OP_MINUS)
-
-def DUMP() -> Token:
-	return Token(TokenTypes.OP_DUMP)
-
-def EXIT(code: int=0) -> Token:
-	return Token(TokenTypes.OP_EXIT, value=code)
+def PUSH(val: Any) -> Token: return Token(TokenTypes.OP_PUSH, val)
+def POP() -> Token: return Token(TokenTypes.OP_POP)
+def PLUS() -> Token: return Token(TokenTypes.OP_PLUS)
+def MINUS() -> Token: return Token(TokenTypes.OP_MINUS)
+def MUL() -> Token: return Token(TokenTypes.OP_MUL)
+def DIV() -> Token: return Token(TokenTypes.OP_DIV)
+def EQ() -> Token: return Token(TokenTypes.OP_EQ)
+def NE() -> Token: return Token(TokenTypes.OP_NE)
+def GT() -> Token: return Token(TokenTypes.OP_GT)
+def GE() -> Token: return Token(TokenTypes.OP_GE)
+def LT() -> Token: return Token(TokenTypes.OP_LT)
+def LE() -> Token: return Token(TokenTypes.OP_LE)
+def DUMP() -> Token: return Token(TokenTypes.OP_DUMP)
+def EXIT(code: int=0) -> Token: return Token(TokenTypes.OP_EXIT, value=code)
 
 def iterqueue(q):
 	while True:
@@ -99,32 +104,101 @@ class Compiler:
 		self.buffer.write(f"  pop  rbp\n")
 
 	def step(self, instruction: Token):
-		assert TokenTypes.OP_COUNT.value == 6, "Not all operators are handled"
+		assert TokenTypes.OP_COUNT.value == 14, f"Not all operators are handled {TokenTypes.OP_COUNT.value}"
 		match instruction:
 			case Token(type=TokenTypes.OP_PUSH, value=val):
 				self.buffer.write(f"  ; push {val}\n")
-				self.buffer.write(f"  push {val:.0f}\n")
+				self.buffer.write(f"  push   {val:.0f}\n")
 			case Token(type=TokenTypes.OP_POP, value=val):
 				self.buffer.write(f"  ; pop\n")
 				self.buffer.write(f"  ; NOT YET IMPLEMENTED\n")
 			case Token(type=TokenTypes.OP_PLUS, value=val):
-				self.buffer.write(f"  ; pop\n")
-				self.buffer.write(f"  pop  rax\n")
-				self.buffer.write(f"  pop  rbx\n")
-				self.buffer.write(f"  add  rax,rbx\n")
-				self.buffer.write(f"  push rax\n")
+				self.buffer.write(f"  ; plus\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  pop    rbx\n")
+				self.buffer.write(f"  add    rax,rbx\n")
+				self.buffer.write(f"  push   rax\n")
+			case Token(type=TokenTypes.OP_MUL, value=val):
+				self.buffer.write(f"  ; mul\n")
+				self.buffer.write(f"  pop    rcx\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  mul    rcx\n")
+				self.buffer.write(f"  push   rax\n")
+			case Token(type=TokenTypes.OP_DIV, value=val):
+				self.buffer.write(f"  ; div\n")
+				self.buffer.write(f"  xor    edx,edx\n")
+				self.buffer.write(f"  pop    rsi\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  div    rsi\n")
+				self.buffer.write(f"  push   rax\n")
 			case Token(type=TokenTypes.OP_MINUS, value=val):
-				self.buffer.write(f"  ; pop\n")
-				self.buffer.write(f"  pop  rax\n")
-				self.buffer.write(f"  pop  rbx\n")
-				self.buffer.write(f"  sub  rbx,rax\n")
-				self.buffer.write(f"  push rbx\n")
+				self.buffer.write(f"  ; minus\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  pop    rbx\n")
+				self.buffer.write(f"  sub    rbx,rax\n")
+				self.buffer.write(f"  push   rbx\n")
+
+			case Token(type=TokenTypes.OP_EQ, value=val):
+				self.buffer.write(f"  ; eq\n")
+				self.buffer.write(f"  xor    rcx,rcx\n")
+				self.buffer.write(f"  mov    rdx,1\n")
+				self.buffer.write(f"  pop    rbx\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  cmp    rbx,rax\n")
+				self.buffer.write(f"  cmove  rcx,rdx\n")
+				self.buffer.write(f"  push   rcx\n")
+			case Token(type=TokenTypes.OP_NE, value=val):
+				self.buffer.write(f"  ; ne\n")
+				self.buffer.write(f"  xor    rcx,rcx\n")
+				self.buffer.write(f"  mov    rdx,1\n")
+				self.buffer.write(f"  pop    rbx\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  cmp    rbx,rax\n")
+				self.buffer.write(f"  cmovne rcx,rdx\n")
+				self.buffer.write(f"  push   rcx\n")
+			case Token(type=TokenTypes.OP_GT, value=val):
+				self.buffer.write(f"  ; gt\n")
+				self.buffer.write(f"  xor    rcx,rcx\n")
+				self.buffer.write(f"  mov    rdx,1\n")
+				self.buffer.write(f"  pop    rbx\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  cmp    rbx,rax\n")
+				self.buffer.write(f"  cmovgt rcx,rdx\n")
+				self.buffer.write(f"  push   rcx\n")
+			case Token(type=TokenTypes.OP_GE, value=val):
+				self.buffer.write(f"  ; ge\n")
+				self.buffer.write(f"  xor    rcx,rcx\n")
+				self.buffer.write(f"  mov    rdx,1\n")
+				self.buffer.write(f"  pop    rbx\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  cmp    rbx,rax\n")
+				self.buffer.write(f"  cmovge rcx,rdx\n")
+				self.buffer.write(f"  push   rcx\n")
+			case Token(type=TokenTypes.OP_LT, value=val):
+				self.buffer.write(f"  ; lt\n")
+				self.buffer.write(f"  xor    rcx,rcx\n")
+				self.buffer.write(f"  mov    rdx,1\n")
+				self.buffer.write(f"  pop    rbx\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  cmp    rbx,rax\n")
+				self.buffer.write(f"  cmovlt rcx,rdx\n")
+				self.buffer.write(f"  push   rcx\n")
+			case Token(type=TokenTypes.OP_LE, value=val):
+				self.buffer.write(f"  ; le\n")
+				self.buffer.write(f"  xor    rcx,rcx\n")
+				self.buffer.write(f"  mov    rdx,1\n")
+				self.buffer.write(f"  pop    rbx\n")
+				self.buffer.write(f"  pop    rax\n")
+				self.buffer.write(f"  cmp    rbx,rax\n")
+				self.buffer.write(f"  cmovle rcx,rdx\n")
+				self.buffer.write(f"  push   rcx\n")
+
 			case Token(type=TokenTypes.OP_DUMP, value=val):
 				self.call_cfunction("__dump", [None])
 			case Token(type=TokenTypes.OP_EXIT, value=val):
 				self.buffer.write(f"  ; EXIT\n")
-				self.buffer.write(f"  mov  rax,60\n")
-				self.buffer.write(f"  mov  rdi,{val}\n")
+				self.buffer.write(f"  mov   rax,60\n")
+				self.buffer.write(f"  mov   rdi,{val}\n")
 				self.buffer.write(f"  syscall\n")
 			case _:
 				raise NotImplemented(instruction)
@@ -136,7 +210,7 @@ class Interpreter:
 	def push(self, v: Any): self.queue.put(v)
 	def pop(self) -> Any: return self.queue.get_nowait()
 	def step(self, instruction: Token):
-		assert TokenTypes.OP_COUNT.value == 6, "Not all operators are handled"
+		assert TokenTypes.OP_COUNT.value == 14, f"Not all operators are handled {TokenTypes.OP_COUNT.value}"
 		match instruction:
 			case Token(type=TokenTypes.OP_PUSH, value=val):
 				self.queue.put(val)
@@ -144,6 +218,32 @@ class Interpreter:
 				self.pop()
 			case Token(type=TokenTypes.OP_PLUS, value=val):
 				self.queue.put(self.pop() + self.pop())
+			case Token(type=TokenTypes.OP_MUL, value=val):
+				self.queue.put(self.pop() * self.pop())
+			case Token(type=TokenTypes.OP_DIV, value=val):
+				self.queue.put(self.pop() // self.pop())
+
+			case Token(type=TokenTypes.OP_EQ, value=val):
+				self.queue.put(int(self.pop() == self.pop()))
+			case Token(type=TokenTypes.OP_NE, value=val):
+				self.queue.put(int(self.pop() != self.pop()))
+			case Token(type=TokenTypes.OP_GT, value=val):
+				b = self.pop()
+				a = self.pop()
+				self.queue.put(int(a > b))
+			case Token(type=TokenTypes.OP_GE, value=val):
+				b = self.pop()
+				a = self.pop()
+				self.queue.put(int(a >= b))
+			case Token(type=TokenTypes.OP_LT, value=val):
+				b = self.pop()
+				a = self.pop()
+				self.queue.put(int(a < b))
+			case Token(type=TokenTypes.OP_LE, value=val):
+				b = self.pop()
+				a = self.pop()
+				self.queue.put(int(a <= b))
+
 			case Token(type=TokenTypes.OP_MINUS, value=val):
 				a = self.pop()
 				b = self.pop()
@@ -169,6 +269,7 @@ class Program:
 
 	@classmethod
 	def frombuffer(cls, buffer):
+		assert TokenTypes.OP_COUNT.value == 14, f"Not all operators are handled {TokenTypes.OP_COUNT.value}"
 		tokens = tokenize.generate_tokens(buffer.readline)
 		self = cls()
 		for token in tokens:
@@ -178,6 +279,14 @@ class Program:
 				case tokenize.TokenInfo(type=tokenize.OP, string=s):
 					if s == '+': self.add(PLUS())
 					elif s == '-': self.add(MINUS())
+					elif s == '*': self.add(MUL())
+					elif s == '/': self.add(DIV())
+					elif s == '==': self.add(EQ())
+					elif s == '!=': self.add(NE())
+					elif s == '>': self.add(GT())
+					elif s == '>=': self.add(GE())
+					elif s == '<': self.add(LT())
+					elif s == '<=': self.add(LE())
 					else: raise UnknownToken(token)
 				case tokenize.TokenInfo(type=tokenize.NAME, string=s):
 					if s == 'dump': self.add(DUMP())
@@ -200,7 +309,7 @@ class Program:
 
 def callcmd(cmd, verbose=False):
 	if verbose:
-		print("BUILD:", cmd)
+		print("CMD:", cmd)
 		return subprocess.call(cmd)
 	else:
 		return subprocess.call(cmd, stdout=subprocess.DEVNULL)
@@ -213,6 +322,7 @@ def main(ac: int, av: list[str]):
 	parser.add_argument("-s", "--source")
 	parser.add_argument("-o", "--output", default="a.out")
 	parser.add_argument("-v", "--verbose", action="store_true")
+	parser.add_argument("-e", "--exec", action="store_true")
 	args = parser.parse_args()
 
 	if args.source:
@@ -221,7 +331,7 @@ def main(ac: int, av: list[str]):
 				p = Program.frombuffer(f)
 			except UnknownToken as e:
 				token: tokenize.TokenInfo = e.args[0]
-				print("\033[31mError: Unknown token:\033[0m\n")
+				print(f"\033[31mError: line {token.start[0]}: Unknown token:\033[0m\n")
 				print(token.line, end='')
 				print(f"{'': <{token.start[1]}}{'^':^<{token.end[1] - token.start[1]}}")
 				exit()
@@ -256,16 +366,13 @@ def main(ac: int, av: list[str]):
 			p.engine = Compiler(f)
 			p.run()
 
-		callcmd(["make", "-C", "src/cfunc/"], verbose=args.verbose)
-		callcmd(["nasm", "-f", "elf64", "objs/intermediary.asm", "-o", "objs/intermediary.o"], verbose=args.verbose)
-		callcmd(["ld",
-				 "src/cfunc/objs/dump.o",
-				 "objs/intermediary.o",
-				 "-lc",
-				 "-I",
-				 "/lib64/ld-linux-x86-64.so.2",
-		   		 "-o",
-		   		 args.output
-			],
-			verbose=args.verbose
-		)
+		if e:=callcmd(["make", "-C", "src/cfunc/"], verbose=args.verbose):
+			exit(e)
+		if e:=callcmd(["nasm", "-f", "elf64", "objs/intermediary.asm", "-o", "objs/intermediary.o"], verbose=args.verbose):
+			exit(e)
+		if e:=callcmd(["ld", "src/cfunc/objs/dump.o", "objs/intermediary.o", "-lc", "-I", "/lib64/ld-linux-x86-64.so.2", "-o", args.output ], verbose=args.verbose):
+			exit(e)
+			
+		if args.exec:
+			if e:=callcmd([f"./{args.output}"], verbose=True):
+				exit(e)
